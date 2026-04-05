67c100cfe9726456e1be41f4884167edbd7bed16
CodeExplorerRay <raymond_rotoni@yahoo.com>
2026-04-05 20:26:55 +0800

    Add missing Spring Boot main class for capitalization-service
    The Maven build was failing because Spring Boot couldn't find a main class.
    Added CapitalizationServiceApplication.java with @SpringBootApplication annotation
    to enable proper JAR packaging and execution.

---
+++ services/capitalization-service/src/main/java/com/helloworld/enterprise/CapitalizationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.helloworld.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalizationServiceApplication.class, args);
+    }
+}
